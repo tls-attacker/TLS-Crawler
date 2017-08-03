e0889ac3f702869f488fc07868f1c1e0a8d503bc
Janis Fliegenschmidt <janisf@gmx.net>
2017-08-03 15:55:18 +0200

    Added Main class and sample unit test

---
+++ src/main/java/de/rub/nds/tlscrawler/Main.java
@@ -0,0 +1,12 @@
+package de.rub.nds.tlscrawler;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println(helloWorld());
+    }
+
+    static String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/de/rub/nds/tlscrawler/MainTest.java
@@ -0,0 +1,13 @@
+package de.rub.nds.tlscrawler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void helloWorldTest() throws Exception {
+        assertEquals("Hello World!", Main.helloWorld());
+    }
+}
