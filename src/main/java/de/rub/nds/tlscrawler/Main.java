69718fe1200bec5bb1f3a7bcec66477b82742bc0
Janis Fliegenschmidt <janisf@gmx.net>
2017-08-30 14:53:35 +0200

    Removed dummy code, added comments

--- src/main/java/de/rub/nds/tlscrawler/Main.java
+++ src/main/java/de/rub/nds/tlscrawler/Main.java
@@ -1,12 +1,20 @@
+/**
+ * TLS Crawler
+ *
+ * Licensed under Apache 2.0
+ *
+ * Copyright 2017 Ruhr-University Bochum
+ */
 package de.rub.nds.tlscrawler;
 
+/**
+ * TLS-Crawler's main class.
+ *
+ * @author janis.fliegenschmidt@rub.de
+ */
 public class Main {
 
     public static void main(String... args) {
-        System.out.println(helloWorld());
-    }
-
-    static String helloWorld() {
-        return "Hello World!";
+        // TODO
     }
 }
