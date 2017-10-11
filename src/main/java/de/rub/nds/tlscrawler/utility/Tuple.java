b52bcc5ac610e750204a89f11b125f37e1de659f
Janis Fliegenschmidt <janisf@gmx.net>
2017-10-11 16:54:46 +0200

    Added generic tuple utility class

---
+++ src/main/java/de/rub/nds/tlscrawler/utility/Tuple.java
@@ -0,0 +1,51 @@
+/**
+ * TLS Crawler
+ *
+ * Licensed under Apache 2.0
+ *
+ * Copyright 2017 Ruhr-University Bochum
+ */
+package de.rub.nds.tlscrawler.utility;
+
+/**
+ * Simple tuple implementation for convenience.
+ *
+ * @param <T1> Type of the first object.
+ * @param <T2> Type of the second object.
+ *
+ * @author janis.fliegenschmidt@rub.de
+ */
+public class Tuple<T1, T2> {
+    public final T1 a;
+    public final T2 b;
+
+    public Tuple(T1 a, T2 b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static <A, B> Tuple<A, B> create(A a, B b) {
+        return new Tuple<A, B>(a, b);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Tuple)) {
+            return false;
+        }
+
+        Tuple<?, ?> p = (Tuple<?, ?>) o;
+
+        return equal(p.a, this.a) && equal(p.b, this.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.a == null ? 0 : this.a.hashCode())
+                ^ (this.b == null ? 0 : this.b.hashCode());
+    }
+
+    private static boolean equal(Object a, Object b) {
+        return a == b || (a != null && a.equals(b));
+    }
+}
