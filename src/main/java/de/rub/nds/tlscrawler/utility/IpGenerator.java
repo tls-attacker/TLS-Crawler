04879e703b616e85836d18a9fe5fa7a587ecde88
Janis Fliegenschmidt <janisf@gmx.net>
2017-10-25 17:36:43 +0200

    edited logging

--- src/main/java/de/rub/nds/tlscrawler/utility/IpGenerator.java
+++ src/main/java/de/rub/nds/tlscrawler/utility/IpGenerator.java
@@ -25,10 +25,14 @@ public class IpGenerator {
         LOG.debug("fullRange() - Start.");
         List<String> result = new ArrayList<>();
 
+        // This is obviously bull*
+        // It serves as a testing device for now, will have to be replaced
+        // Idea: Implement as a JavaRX source -- backpressured stream
         for (int a = 0; a < 256; a++) {
+            LOG.debug("fullRange() - Outermost Loop.");
             for (int b = 0; b < 256; b++) {
-                for (int c = 0; c < 256; c++) {
-                    for (int d = 0; d < 256; d++) {
+                for (int c = 0; c < 1; c++) {
+                    for (int d = 0; d < 1; d++) {
                         result.add(String.format("%d.%d.%d.%d", a, b, c, d));
                     }
                 }
